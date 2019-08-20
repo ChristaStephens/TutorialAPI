b8d56916a015e91c337b724725cfb0ce1ad92d37
Christa Stephens <christastephens@quickenloans.com>
2019-08-20 13:41:42 -0400

    added notes to db class/command context. remove app settings notes

--- Models/CommandContext.cs
+++ Models/CommandContext.cs
@@ -2,7 +2,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace TutorialAPI.Models
 {
-    //inherit from DbContext class.
+    //inherits from DbContext class.
+    //we need to make this class aware of our appsettings that
+    //are connecting to our database/sql
+
+    //our appsettings.json is our connection to the server
+    //it contains the name of the sql server name "connection string"
     public class CommandContext : DbContext
     {
         //allows you to pass in options when creating DBcontext
