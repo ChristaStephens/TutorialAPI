5d343e8548d4d01bb12cf5c7cf5ad9f8ed53dbf3
Christa Stephens <christastephens@quickenloans.com>
2019-08-20 12:20:29 -0400

    tested get method, works on google and postman

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -5,14 +5,22 @@ namespace TutorialAPI.Controllers
 {
     //Commands Controller is a class that inherits from Controller base
     //ControllerBase is a basic class for an MVC controller without a view.
+    //need to decorate the controller with [ApiController] so that the MVC
+    //framework can recognize it as a controller class
+    //we also need to give it a route (like a path)
+    //so that we know where to call the method. Think of a url.
+    //you can use [controller] or the first name of the controller folder in lowercase
+    //just remember to use that when typing the path into postman/google
+    [Route("api/[controller]")]
+    [ApiController]
     public class CommandsController : ControllerBase
     {
         //Create an action result, maps to our crud methods and swagger
-       // an endpoint that will return a string
+        // an endpoint that will return a string
         [HttpGet]
         public ActionResult<IEnumerable<string>> GetString()
         {
-            return new string [] {"this", "is", "a", "placeholder"};
+            return new string[] { "this", "is", "a", "placeholder" };
         }
     }
 }
